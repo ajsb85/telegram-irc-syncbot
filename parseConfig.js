db91924af93e0908d6994a792bd858a40cc92108
Gleki Arxokuna <gleki.is.my.name@gmail.com>
2016-01-19 19:57:45 +0300

    Update parseConfig.js

--- parseConfig.js
+++ parseConfig.js
@@ -1,96 +1,7 @@
 var _ = require('lodash');
 var fs = require('fs');
 var mkdirp = require('mkdirp');
-var defaultConfig = require('./config.defaults');
-
-var warnDeprecated = function(oldOpt, newOpt) {
-    console.warn('WARNING: detected usage of deprecated config option "' + oldOpt + '", ' +
-                 'support for it will be dropped in a future version! Consider ' +
-                 'migrating to the new "' + newOpt + '" option instead.');
-};
-
-// support old config options for a few versions, but warn about their usage
-var parseDeprecatedOptions = function(config) {
-
-    // check the contents of the channels array for old options
-    _.forEach(config.channels, function(channel, i) {
-        _.forEach(channel, function(value, key) {
-            if (key === 'irc_channel') {
-                config.channels[i].chanAlias = value;
-                warnDeprecated(key, 'chanAlias');
-                return;
-            }
-            if (key === 'irc_channel_id') {
-                config.channels[i].ircChan = value;
-                warnDeprecated(key, 'ircChan');
-                return;
-            }
-            if (key === 'irc_channel_pwd') {
-                config.channels[i].chanPwd = value;
-                warnDeprecated(key, 'chanPwd');
-                return;
-            }
-            if (key === 'tg_chat') {
-                config.channels[i].tgGroup = value;
-                warnDeprecated(key, 'tgGroup');
-                return;
-            }
-        });
-    });
-
-    // search for old config options
-    _.forEach(config, function(value, option) {
-        if (option === 'tg_bot_token') {
-            config.tgToken = value;
-            warnDeprecated(option, 'tgToken');
-            return;
-        }
-        if (option === 'irc_nick') {
-            config.ircNick = value;
-            warnDeprecated(option, 'ircNick');
-            return;
-        }
-        if (option === 'irc_server') {
-            config.ircServer = value;
-            warnDeprecated(option, 'ircServer');
-            return;
-        }
-        if (option === 'irc_options') {
-            config.ircOptions = value;
-            warnDeprecated(option, 'ircOptions');
-            return;
-        }
-        if (option === 'irc_relay_all') {
-            config.ircRelayAll = value;
-            warnDeprecated(option, 'ircRelayAll');
-            return;
-        }
-        if (option === 'irc_hilight_re') {
-            config.hlRegexp = value;
-            warnDeprecated(option, 'hlRegexp');
-            return;
-        }
-        if (option === 'send_topic') {
-            config.sendTopic = value;
-            warnDeprecated(option, 'sendTopic');
-            return;
-        }
-    });
-
-    return config;
-};
-
-if (process.argv[2] === '--genconfig') {
-    mkdirp(process.env.HOME + '/.teleirc');
-
-    // read default config using readFile to include comments
-    var config = fs.readFileSync(__dirname + '/config.defaults.js');
-    var configPath = process.env.HOME + '/.teleirc/config.js';
-    fs.writeFileSync(configPath, config);
-    console.log('Wrote default configuration to ' + configPath +
-                ', please edit it before re-running');
-    process.exit(0);
-}
+var defaultConfig = require('./tg_config.defaults');
 
 module.exports = function() {
     var config;
@@ -104,7 +15,6 @@ module.exports = function() {
         process.exit(1);
     }
 
-    config = parseDeprecatedOptions(config);
     config = _.defaults(config, defaultConfig);
 
     return config;
