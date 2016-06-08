4c00b7b0aec0a7fe700b5fadced83e5500e9b87d
Gleki Arxokuna <gleki.is.my.name@gmail.com>
2016-06-08 13:37:23 +0300

    Update parseConfig.js

--- parseConfig.js
+++ parseConfig.js
@@ -39,8 +39,8 @@ module.exports = function() {
 	        config.usernameFallbackFormat = '%firstName% %lastName%';
 	        config.ircNick = 'ttttt';//process.env['TELEGRAMBOT_ircNick'];
 	        config.ircServer = 'irc.freenode.net';//process.env['TELEGRAMBOT_ircServer'];
-	        config.channels = [JSON.parse(process.env['TELEGRAMBOT_channels'])];
-	        config.ircOptions = JSON.parse(process.env['TELEGRAMBOT_ircOptions']);
+	        config.channels = [JSON.parse(process.env['CHANNELS'])];
+	        config.ircOptions = JSON.parse(process.env['OPTIONS']);
 	        config.ircRelayAll = true;//process.env['TELEGRAMBOT_ircRelayAll'];
 	    }
 	    catch(e){
